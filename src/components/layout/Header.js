1e809d4070088b24ee5992e50066f8d18d6c4472
Zach Smith <zsmith126028@gmail.com>
2019-04-19 11:40:50 -0500

    add Header file in components/layout directory

---
+++ src/components/layout/Header.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+const Header = (props) => {
+  const { branding } = props;
+  
+  return (
+    <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-3">
+      <div className="container">
+        <a href="#" className="navbar-brand">{branding}</a>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link to="#" className="nav-link">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="#" className="nav-link">About</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+Header.propTypes = {
+  branding: PropTypes.string.isRequired
+}
+
+Header.defaultProps = {
+  branding: 'Shadow Pics'
+}
+
+export default Header;
