e3cbe445ca9748e7e43517ca6810b8a355d05b18
Zach Smith <zsmith126028@gmail.com>
2019-04-19 17:12:13 -0500

    add Picture to Pictures page

--- src/components/posts/Pictures.js
+++ src/components/posts/Pictures.js
@@ -5,6 +5,7 @@ class Pictures extends Component {
   render() {
     return (
       <div>
+        <Picture />
         <h2>This will be the page that lists all the picture posts</h2>
       </div>
     )
