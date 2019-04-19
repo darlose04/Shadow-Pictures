ad2ebcf63b8362e1d4bd27e1b09c9cd686a81bec
Zach Smith <zsmith126028@gmail.com>
2019-04-19 11:41:14 -0500

    add Header link

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from "./components/layout/Header";
 
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>This is going to be a site for pictures of Shadow, my dog.</h1>
-      </div>
+      <Router>
+        <div className="App">
+          <Header branding="Image Gallery" />
+          <h1>This is going to be a site for pictures of Shadow, my dog.</h1>
+        </div>
+      </Router>
     );
   }
 }
