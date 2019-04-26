33095d3d0dec152d0fd682e51e1a7006c7d89561
Zach Smith <zsmith126028@gmail.com>
2019-04-26 16:14:58 -0500

    add about component

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>This is an app dedicated to showing picures of my dog, Shadow. Shadow is half German Shepherd, half Husky and is 2 years old.</p>
+      <p>Version 1.0.0</p>
+    </React.Fragment>
+  )
+}
+
+export default About;
