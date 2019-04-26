b0d4423751b1303814a794be197d8e13eb542f2b
Zach Smith <zsmith126028@gmail.com>
2019-04-26 17:20:45 -0500

    remove title

--- src/components/posts/Picture.js
+++ src/components/posts/Picture.js
@@ -3,13 +3,13 @@ import PropTypes from 'prop-types';
 
 class Picture extends Component {
   render() {
-    const { id, title, desc, image } = this.props.picture;
+    const { id, desc, image } = this.props.picture;
     return (
       <div className="container">
         <div className="card mb-3">
         <img src={image} alt="dog pics" className="card-img-top"/>
           <div className="card-body">
-            <h4 className="card-title">{title}</h4>
+            {/* <h4 className="card-title">{title}</h4> */}
             <p className="card-text">{desc}</p>
           </div>
         </div>
