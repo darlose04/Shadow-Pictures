2012d25ddf2b049cc23257b9af09682933810018
Zach Smith <zsmith126028@gmail.com>
2019-04-25 23:54:47 -0500

    add card elements to display image (not working), title, and description

--- src/components/posts/Picture.js
+++ src/components/posts/Picture.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Picture extends Component {
   render() {
+    const { id, title, desc, image } = this.props.picture;
     return (
-      <div>
-        <h2>This is going to detail how posts look</h2>
+      <div className="container">
+        <div className="card mb-3">
+        <img src={image} alt="" className="card-img-top"/>
+          <div className="card-body">
+            <h4 className="card-title">{title}</h4>
+            <p className="card-text">{desc}</p>
+          </div>
+        </div>
       </div>
     )
   }
+}
+
+Picture.propTypes = {
+  picture: PropTypes.object.isRequired
 }
 
 export default Picture;
