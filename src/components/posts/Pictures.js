abcfa73e02b3f39bb63f5898da8107a91d7a7bf7
Zach Smith <zsmith126028@gmail.com>
2019-04-25 22:34:07 -0500

    create map function to iterate through pictures in the state object

--- src/components/posts/Pictures.js
+++ src/components/posts/Pictures.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import Picture from "./Picture";
+import PropTypes from 'prop-types';
 
 class Pictures extends Component {
   render() {
-    return (
-      <div>
-        <Picture />
-        <h2>This will be the page that lists all the picture posts</h2>
-      </div>
-    )
+    return this.props.pictures.map((picture) => {
+      <Picture key={picture.id} picture={picture} />
+    });
   }
+}
+
+Pictures.propTypes = {
+  pictures: PropTypes.array.isRequired
 }
 
 export default Pictures;
