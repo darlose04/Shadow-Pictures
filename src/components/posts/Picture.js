486d200897a1c111f43b7cdb568ad4139852eb79
Zach Smith <zsmith126028@gmail.com>
2019-04-27 11:20:16 -0500

    add delete function and import Consumer from context

--- src/components/posts/Picture.js
+++ src/components/posts/Picture.js
@@ -1,19 +1,40 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Consumer } from '../../context';
 
 class Picture extends Component {
+
+  onDeleteClick = async (id, dispatch) => {
+    try {
+      await dispatch({type: 'DELETE_PICTURE', payload: id});
+    } catch(e) {
+      dispatch({type: 'DELETE_PICTURE', payload: id});
+    }
+  }
+
   render() {
     const { id, desc, image } = this.props.picture;
     return (
-      <div className="container">
-        <div className="card mb-3">
-        <img src={image} alt="dog pics" className="card-img-top"/>
-          <div className="card-body">
-            {/* <h4 className="card-title">{title}</h4> */}
-            <p className="card-text">{desc}</p>
-          </div>
-        </div>
-      </div>
+      <Consumer>
+        {value => {
+          const { dispatch } = value;
+          return (
+            <div className="container">
+              <div className="card mb-3">
+              <img src={image} alt="dog pics" className="card-img-top"/>
+                <div className="card-body">
+                  {/* <h4 className="card-title">{title}</h4> */}
+                  <p className="card-text">
+                    {desc}
+                    <i className="fas fa-trash-alt" style={{cursor: 'pointer', float:'right', color:'red'}} onClick={this.onDeleteClick.bind(this, id, dispatch)}></i>
+                  </p>
+                </div>
+              </div>
+            </div>
+          )
+        }}
+        
+      </Consumer>
     )
   }
 }
