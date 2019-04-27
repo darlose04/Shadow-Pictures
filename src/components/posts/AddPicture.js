4ce5a1ffb576fc6bb1c57042623cdd97bda62265
Zach Smith <zsmith126028@gmail.com>
2019-04-26 20:30:27 -0500

    add onChange and onSubmit

--- src/components/posts/AddPicture.js
+++ src/components/posts/AddPicture.js
@@ -7,14 +7,23 @@ class AddPicture extends Component {
     image: ''
   }
 
+  onSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(this.state);
+  }
+
+  onChange = (e) => this.setState({ [e.target.name]: e.target.value });
+
   render() {
+    const { desc, image } = this.state;
     return (
       <div className="card mb-3">
-        <div className="card-header">
+        <div className="card-header bg-dark text-light">
           Add Picture
         </div>
         <div className="card-body">
-          <form>
+          <form onSubmit={this.onSubmit}>
             <div className="form-group">
               <label htmlFor="desc">Description</label>
               <input 
@@ -22,13 +31,18 @@ class AddPicture extends Component {
                 name="desc"
                 className="form-control"
                 placeholder="Enter description..."
+                value={desc}
+                onChange={this.onChange}
               />
             </div>
             <div className="form-group">
               <label htmlFor="image">Add image</label>
               <input 
                 type="file"
+                name="image"
                 className="form-control-file"
+                value={image}
+                onChange={this.onChange}
               />
             </div>
             <input type="submit" value="Add Picture" className="btn btn-block btn-primary"/>
