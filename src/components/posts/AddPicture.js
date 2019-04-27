981258a9e7349cb0ace0d2d57bd5612e5609b6c8
Zach Smith <zsmith126028@gmail.com>
2019-04-27 18:35:01 -0500

    doesn't work because uploading local images is a pain

--- src/components/posts/AddPicture.js
+++ src/components/posts/AddPicture.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
+import { Consumer } from '../../context';
+import uuid from 'uuid';
 
 class AddPicture extends Component {
   state = {
@@ -7,10 +9,18 @@ class AddPicture extends Component {
     image: ''
   }
 
-  onSubmit = (e) => {
+  onSubmit = (dispatch, e) => {
     e.preventDefault();
 
-    console.log(this.state);
+    const { desc, image } = this.state;
+
+    const newPicture = {
+      id: uuid.v4(),
+      desc,
+      image: require()
+    }
+
+    dispatch({type: 'ADD_CONTACT', payload: newPicture})
 
     // clear state
     this.setState({
@@ -23,38 +33,46 @@ class AddPicture extends Component {
 
   render() {
     const { desc, image } = this.state;
+
     return (
-      <div className="card mb-3">
-        <div className="card-header bg-dark text-light">
-          Add Picture
-        </div>
-        <div className="card-body">
-          <form onSubmit={this.onSubmit}>
-            <div className="form-group">
-              <label htmlFor="desc">Description</label>
-              <input 
-                type="text" 
-                name="desc"
-                className="form-control"
-                placeholder="Enter description..."
-                value={desc}
-                onChange={this.onChange}
-              />
+      <Consumer>
+        {value => {
+          const { dispatch } = value;
+          return (
+            <div className="card mb-3">
+              <div className="card-header bg-dark text-light">
+                Add Picture
+              </div>
+              <div className="card-body">
+                <form onSubmit={this.onSubmit.bind(this, dispatch)}>
+                  <div className="form-group">
+                    <label htmlFor="desc">Description</label>
+                    <input 
+                      type="text" 
+                      name="desc"
+                      className="form-control"
+                      placeholder="Enter description..."
+                      value={desc}
+                      onChange={this.onChange}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="image">Add image</label>
+                    <input 
+                      type="file"
+                      name="image"
+                      className="form-control-file"
+                      value={image}
+                      onChange={this.onChange}
+                    />
+                  </div>
+                  <input type="submit" value="Add Picture" className="btn btn-block btn-primary"/>
+                </form>
+              </div>
             </div>
-            <div className="form-group">
-              <label htmlFor="image">Add image</label>
-              <input 
-                type="file"
-                name="image"
-                className="form-control-file"
-                value={image}
-                onChange={this.onChange}
-              />
-            </div>
-            <input type="submit" value="Add Picture" className="btn btn-block btn-primary"/>
-          </form>
-        </div>
-      </div>
+          )
+        }}
+      </Consumer>
     )
   }
 }
