9f641dc701b9e992d64c93cfdd49af3ab154205f
Zach Smith <zsmith126028@gmail.com>
2019-04-27 10:51:26 -0500

    add Consumer from context and adjust component return

--- src/components/posts/Pictures.js
+++ src/components/posts/Pictures.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
+import { Consumer } from '../../context';
 import Picture from "./Picture";
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 class Pictures extends Component {
   render() {
-    return this.props.pictures.map((picture) => (
-      <Picture key={picture.id} picture={picture} />
-    ));
+    return (
+      <Consumer>
+        {value => {
+          const { pictures } = value;
+          return (
+            <React.Fragment>
+              <h1 className="display-4 mb-2">Posts</h1>
+              {pictures.map(picture => (
+                <Picture 
+                  key={picture.id}
+                  picture={picture}
+                />
+              ))}
+            </React.Fragment>
+          )
+        }}
+      </Consumer>
+    );
   }
-}
-
-Pictures.propTypes = {
-  pictures: PropTypes.array.isRequired
 }
 
 export default Pictures;
